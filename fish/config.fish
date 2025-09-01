70f785055ff57ecaee98b0d3356f88f438009ead
Lepotototor <victor.flament@epita.fr>
2025-09-01 08:09:46 +0200

    [REMOVE] neofetch at start

--- fish/config.fish
+++ fish/config.fish
@@ -148,4 +148,4 @@ alias jctl="journalctl -p 3 -xb"
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 #neofetch --ascii ~/.config/fish/ascii_neofetch
-neofetch
+# neofetch
